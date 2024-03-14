1d73297f2acf729a4ca678feec2fdac4faf8c3d4
A-Pogam <alicepogam59@hotmail.com>
2024-03-14 17:12:48 +0100

    Created test for app

--- dataLayer/src/test/java/com/paymybuddy/dataLayer/DataLayerApplicationTests.java
+++ dataLayer/src/test/java/com/paymybuddy/dataLayer/DataLayerApplicationTests.java
@@ -2,12 +2,22 @@ package com.paymybuddy.dataLayer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import controller.LoginController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 @SpringBootTest
 class DataLayerApplicationTests {
 
+	@Autowired
+	private LoginController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads()throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
