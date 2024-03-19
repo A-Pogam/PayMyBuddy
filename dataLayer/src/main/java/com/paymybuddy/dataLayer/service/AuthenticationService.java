812727b56f71e48900dd5d66b34962d29af9b763
A-Pogam <alicepogam59@hotmail.com>
2024-03-19 19:16:40 +0100

    Cleaned AuthService

--- dataLayer/src/main/java/com/paymybuddy/dataLayer/service/AuthenticationService.java
+++ dataLayer/src/main/java/com/paymybuddy/dataLayer/service/AuthenticationService.java
@@ -1,6 +1,5 @@
 package com.paymybuddy.dataLayer.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -8,20 +7,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthenticationService {
 
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public AuthenticationService(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     public boolean authenticate(String email, String enteredPassword) {
+        // Utilise userDetailsService.loadUserByUsername(email) et le reste de votre logique d'authentification ici
         UserDetails userDetails = userDetailsService.loadUserByUsername(email);
         if (userDetails == null) {
             return false; // L'utilisateur n'existe pas
         }
 
-        // Récupérer les informations de l'utilisateur
+        // Récupère les informations de l'utilisateur
         String encodedPassword = userDetails.getPassword(); // Mot de passe encodé
         GrantedAuthority userRole = userDetails.getAuthorities().iterator().next(); // Rôle de l'utilisateur
 
-        // Comparer les mots de passe
+        // Compare les mots de passe
         return userDetailsService.passwordsMatch(enteredPassword, encodedPassword);
     }
 }
