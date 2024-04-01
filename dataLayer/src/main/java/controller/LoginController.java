4afc140cdf87310b13da944fc8cc2d32f56dc968
A-Pogam <alicepogam59@hotmail.com>
2024-04-01 15:54:34 +0200

    Deleted htmlcontroller + modified controllers

--- dataLayer/src/main/java/controller/LoginController.java
+++ dataLayer/src/main/java/controller/LoginController.java
@@ -4,21 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import util.HTMLGenerator;
 
 @Controller
 public class LoginController {
 
-    private final HTMLGenerator htmlGenerator;
-
-    @Autowired
-    public LoginController(HTMLGenerator htmlGenerator) {
-        this.htmlGenerator = htmlGenerator;
-    }
-
     @GetMapping("/login")
-    @ResponseBody
-    public String loginPage() {
-        return "login";
+    public String login() {
+        return "login"; 
     }
 }
