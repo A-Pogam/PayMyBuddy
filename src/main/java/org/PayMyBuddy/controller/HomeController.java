e3a5fd5326c536030818f7d09cac62408a226a54
A-Pogam <alicepogam59@hotmail.com>
2024-04-02 16:22:12 +0200

    added log4j to homecontroller

--- src/main/java/org/PayMyBuddy/controller/HomeController.java
+++ src/main/java/org/PayMyBuddy/controller/HomeController.java
@@ -1,15 +1,21 @@
 package org.PayMyBuddy.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Controller
 public class HomeController {
 
+    private final Logger logger = LogManager.getLogger(HomeController.class);
+
     @GetMapping("/home")
     public String home() {
+        logger.info("Received request to access home page.");
         return "home";
     }
-
 }
