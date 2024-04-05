a2e4cda9be7475870726f3eaca48b3b9ebd20399
A-Pogam <alicepogam59@hotmail.com>
2024-04-05 16:22:25 +0200

    improved profile & created create.sql

---
+++ src/main/resources/create.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS paymybuddy_db;
+
+CREATE DATABASE paymybuddy_db;
+USE paymybuddy_db;
+
+CREATE TABLE dbuser (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100),
+    role ENUM('USER', 'ADMIN') DEFAULT 'USER',
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    balance DECIMAL(10, 2)
+);
+
+CREATE TABLE connection (
+    connection_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fk_initializer_id INT NOT NULL,
+    fk_receiver_id INT NOT NULL,
+    starting_date DATETIME NOT NULL,
+    FOREIGN KEY (fk_initializer_id)
+        REFERENCES dbuser (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (fk_receiver_id)
+        REFERENCES dbuser (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE transaction (
+    transaction_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fk_issuer_id INT NOT NULL,
+    fk_payee_id INT NOT NULL,
+    date DATETIME NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(140),
+    FOREIGN KEY (fk_issuer_id)
+        REFERENCES dbuser (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (fk_payee_id)
+        REFERENCES dbuser (id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
