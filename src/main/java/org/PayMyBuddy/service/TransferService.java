779f1405774c89659e985076a0f825e8a7009b39
A-Pogam <alicepogam59@hotmail.com>
2024-04-18 17:26:33 +0200

    added 0.5% free/transaction

--- src/main/java/org/PayMyBuddy/service/TransferService.java
+++ src/main/java/org/PayMyBuddy/service/TransferService.java
@@ -24,7 +24,6 @@ public class TransferService {
         // Récupérer l'utilisateur expéditeur de la base de données
         Optional<DBUser> senderOptional = dbUserRepository.findById(sender_id);
         if (senderOptional.isEmpty()) {
-            // Gérer le cas où l'utilisateur expéditeur n'est pas trouvé
             throw new RuntimeException("Sender not found");
         }
         DBUser sender = senderOptional.get();
@@ -32,7 +31,6 @@ public class TransferService {
         // Récupérer l'utilisateur destinataire de la base de données
         Optional<DBUser> receiverOptional = dbUserRepository.findById(receiver_id);
         if (receiverOptional.isEmpty()) {
-            // Gérer le cas où l'utilisateur destinataire n'est pas trouvé
             throw new RuntimeException("Receiver not found");
         }
         DBUser receiver = receiverOptional.get();
@@ -40,26 +38,33 @@ public class TransferService {
         // Vérifier si l'expéditeur a suffisamment de fonds pour effectuer la transaction
         BigDecimal senderBalance = sender.getBalance();
         if (senderBalance.compareTo(amount) < 0) {
-            // Gérer le cas où l'expéditeur n'a pas suffisamment de fonds
             throw new RuntimeException("Insufficient funds");
         }
 
+        // Calculer le montant du prélèvement (0,5 %)
+        BigDecimal feePercentage = BigDecimal.valueOf(0.5);
+        BigDecimal feeAmount = amount.multiply(feePercentage.divide(BigDecimal.valueOf(100)));
+
+        // Calculer le montant final à transférer au destinataire (montant de la transaction - montant du prélèvement)
+        BigDecimal finalAmount = amount.subtract(feeAmount);
+
         // Déduire le montant de la balance de l'expéditeur
-        BigDecimal newSenderBalance = senderBalance.subtract(amount);
+        BigDecimal newSenderBalance = senderBalance.subtract(finalAmount); 
         sender.setBalance(newSenderBalance);
         dbUserRepository.save(sender);
 
         // Mettre à jour la balance du destinataire
-        updateReceiverBalance(receiver, amount);
+        updateReceiverBalance(receiver, finalAmount);
 
         // Enregistrer la transaction dans la base de données
         Transaction transaction = new Transaction();
         transaction.setSender_id(sender_id);
         transaction.setReceiver_id(receiver_id);
         transaction.setDescription(description);
-        transaction.setAmount(amount);
+        transaction.setAmount(finalAmount);
         transactionRepository.save(transaction);
     }
+
 
     private void updateReceiverBalance(DBUser receiver, BigDecimal amount) {
         BigDecimal receiverBalance = receiver.getBalance();
