135dadd7213acd9a327c8c40b0b17f7331b5d771
A-Pogam <alicepogam59@hotmail.com>
2024-05-23 21:11:21 +0200

    Improved HomeControllerTest

--- src/test/java/org/PayMyBuddy/controller/HomeControllerTest.java
+++ src/test/java/org/PayMyBuddy/controller/HomeControllerTest.java
@@ -1,94 +1,98 @@
 package org.PayMyBuddy.controller;
 
+import jakarta.servlet.ServletException;
 import org.PayMyBuddy.model.User;
 import org.PayMyBuddy.service.contracts.IUserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.Model;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = HomeController.class)
 class HomeControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private IUserService userService;
 
-    @Mock
+    @MockBean
     private Authentication authentication;
 
-    @Mock
+    @MockBean
     private SecurityContext securityContext;
 
-    @Mock
+    @MockBean
     private UserDetails userDetails;
 
-    @Mock
+    @MockBean
     private Model model;
 
     @InjectMocks
     private HomeController homeController;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
-    void testHome() {
+    @WithMockUser(username = "user@example.com", roles = { "USER" })
+    void testHome() throws Exception {
         // Given
         String userEmail = "user@example.com";
-        User user = new User();  // Assurez-vous que le constructeur et les setters de User sont accessibles
+        User user = new User();
         user.setEmail(userEmail);
 
-        // Simuler l'authentification
+        // when
         when(securityContext.getAuthentication()).thenReturn(authentication);
         when(authentication.getPrincipal()).thenReturn(userDetails);
         when(userDetails.getUsername()).thenReturn(userEmail);
         SecurityContextHolder.setContext(securityContext);
 
-        // Simuler le service utilisateur
         when(userService.findByEmail(userEmail)).thenReturn(Optional.of(user));
 
-        // When
-        String actualView = homeController.home(model);
+        //  Then
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("user", user));
 
-        // Then
-        assertEquals("home", actualView);
         verify(userService, times(1)).findByEmail(userEmail);
-        verify(model, times(1)).addAttribute("user", user);
     }
 
     @Test
-    void testHomeUserNotFound() {
+    @WithMockUser(username = "user@example.com", roles = { "USER" })
+    void testHomeUserNotFound() throws Exception {
         // Given
         String userEmail = "user@example.com";
 
-        // Simuler l'authentification
+        //  When
         when(securityContext.getAuthentication()).thenReturn(authentication);
         when(authentication.getPrincipal()).thenReturn(userDetails);
         when(userDetails.getUsername()).thenReturn(userEmail);
         SecurityContextHolder.setContext(securityContext);
 
-        // Simuler le service utilisateur
         when(userService.findByEmail(userEmail)).thenReturn(Optional.empty());
 
-        // When
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> homeController.home(model));
-
-        // Then
-        assertEquals("User not found", exception.getMessage());
-        verify(userService, times(1)).findByEmail(userEmail);
-        verify(model, never()).addAttribute(anyString(), any());
+        //  Then
+        assertThrows(ServletException.class, () -> mockMvc.perform(get("/home")));
     }
+
+
 }
