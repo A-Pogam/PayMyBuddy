21f3ee8a78de3bd14da04c1b7e36648b9d52743f
A-Pogam <alicepogam59@hotmail.com>
2024-06-06 15:59:39 +0200

    updated script with new connexion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Insertion de données dans la table user
 INSERT INTO user (user_id, user_email, user_password, user_firstname, user_lastname, user_role, user_balance) VALUES
-(1, 'user@example.com', 'user', 'User', 'X', 'USER', 200.00),
-(2, 'clara@example.com', 'clara', 'Clara', 'X', 'USER', 0.00);
+(1, 'user@example.com', '$2y$10$dp.FpiGkFeb0WeA.VstKhuOZo2c79yO5eXwlk.YODQpfYc4ObM0EC', 'User', 'X', 'USER', 200.00),
+(2, 'clara@example.com', '$2y$10$hhCrJNIRPrjSJtu1avH8euh/mWEw7cF4uO1sX6Vz4LfkLgNBTOB6W', 'Clara', 'X', 'USER', 0.00);
 
 -- Insertion de données dans la table contact
 INSERT INTO contact (contact_first_user_id, contact_second_user_id) VALUES
